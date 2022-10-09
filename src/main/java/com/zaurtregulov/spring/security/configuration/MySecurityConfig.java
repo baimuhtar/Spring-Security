683b8476e0756682437d543eedaadf0b74b96f6d
baimuhtar <baimuhtar1997@gmail.com>
2022-10-09 10:55:39 +0600

    storing passwords in a database in unencrypted form

--- src/main/java/com/zaurtregulov/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/zaurtregulov/spring/security/configuration/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package com.zaurtregulov.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,17 +8,28 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+
 
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
 
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("zaur").password("zaur").roles("EMPLOYEE"))
-                .withUser(userBuilder.username("elena").password("elena").roles("HR"))
-                .withUser(userBuilder.username("ivan").password("ivan").roles("MANAGER", "HR"));
+        auth.jdbcAuthentication().dataSource(dataSource);
+
+
+
+//        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//
+//        auth.inMemoryAuthentication()
+//                .withUser(userBuilder.username("zaur").password("zaur").roles("EMPLOYEE"))
+//                .withUser(userBuilder.username("elena").password("elena").roles("HR"))
+//                .withUser(userBuilder.username("ivan").password("ivan").roles("MANAGER", "HR"));
 
     }
 
